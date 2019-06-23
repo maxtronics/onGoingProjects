1c7e20d2450587891dad61310b977b6044a2a426
maxtronics <50552423+maxtronics@users.noreply.github.com>
2019-06-23 12:18:42 +0800

    Add files via upload

---
+++ FingerPrint_web/FingerPrint_web.ino
@@ -0,0 +1,86 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+
+
+#include <ESP8266HTTPClient.h>
+
+#define USE_SERIAL Serial
+
+ESP8266WiFiMulti WiFiMulti;
+
+
+
+String payload = "";
+
+void setup() {
+  USE_SERIAL.begin(115200);
+   // USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+    for(uint8_t t = 4; t > 0; t--) {
+        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
+
+    WiFiMulti.addAP("HUAWEIKO", "maxim123456");
+}
+
+void sendDataToWeb(String fid){
+
+ 
+ if((WiFiMulti.run() == WL_CONNECTED)) {
+
+        HTTPClient http;
+
+        USE_SERIAL.print("[HTTP] begin...\n");
+        // configure traged server and url
+        String url = "http://maxtronics.x10host.com/templates/fid.php?varA=" + fid;
+        //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
+        http.begin(url); //HTTP
+
+        USE_SERIAL.print("[HTTP] GET...\n");
+        // start connection and send HTTP header
+        int httpCode = http.GET();
+
+        // httpCode will be negative on error
+        if(httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+
+            // file found at server
+            if(httpCode == HTTP_CODE_OK) {
+                payload = http.getString();
+                int startIndex = payload.indexOf("001100");
+                int stopIndex  = payload.indexOf("110011");
+                payload = payload.substring(startIndex + 6, stopIndex);
+                String dataToSend = payload;
+                dataToSend = "110011|" + dataToSend +  "|11:20PM| May 29, 2019!" + "001100";
+                USE_SERIAL.print("LOGGED: ");
+                USE_SERIAL.println(dataToSend);
+            }
+        } else {
+            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+    }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(USE_SERIAL.available() > 0){
+    String data = Serial.readStringUntil('\n');
+    Serial.println(data);
+
+    sendDataToWeb(data);
+  }
+}
+
+
---
+++ oneWayTrafficLight/Terminal_2/Terminal_2.ino
@@ -0,0 +1,171 @@
+/*
+* Arduino Wireless Communication Tutorial
+*       Example 1 - Receiver Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+
+unsigned long previousMillis = 0;  
+
+const long interval = 30000;
+unsigned long currentMillis;
+
+int timer = 0;
+bool overrideState = true;
+int counter;
+
+int btnFlag;
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);
+
+#define btn 3
+#define redLed 4
+#define greenLed 5
+//#define vccPin 2
+
+
+RF24 radio(7, 8); // CE, CSN
+const byte address_uno2Mega[6] = "00001";
+const byte address_Mega2Uno[6] = "00002";
+
+void setup() {
+
+   lcd.init();
+  
+  lcd.backlight();
+  
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address_Mega2Uno);
+  radio.openWritingPipe(address_uno2Mega);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.startListening();
+
+  pinMode(btn, INPUT_PULLUP);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+//  pinMode(vccPin, OUTPUT);
+
+  digitalWrite(greenLed, HIGH);
+    digitalWrite(redLed, LOW);
+
+ // digitalWrite(vccPin, HIGH);
+}
+
+void printTimer(String color, int timer){
+  lcd.setCursor(0, 0);
+  String colorVal = "COLOR: " + color + "     ";
+  lcd.print(colorVal);
+  lcd.setCursor(0, 1);
+  String timerVal = "TIME: " + String(timer) + "           ";
+
+  Serial.println(colorVal);
+  Serial.println(timerVal);
+  lcd.print(timerVal);
+  
+}
+
+void loop() {
+int btnState = digitalRead(btn);
+if(btnFlag == 0){             
+        if(btnState == 0){
+          btnFlag = 1;
+          delay(100);
+        }
+      }
+    
+      if(btnFlag == 1){              
+       if(btnState == 1){
+        btnFlag = 0;
+        delay(10);
+          counter = counter + 1;
+        //send radio data
+          radio.stopListening();
+          String txt = "COUNTER: " + String(counter);
+          char text[16];
+          txt.toCharArray(text, 16);
+          Serial.print("TEXT: ");
+          Serial.print(text);
+          Serial.println("\n");
+          radio.write(&text, sizeof(text));
+          Serial.println("Data Sent");
+   // delay(1000);
+        }
+        
+      }
+
+      currentMillis = millis();
+
+if(overrideState == true){
+      if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    
+     radio.stopListening();
+     String txt = "COUNTER: " + String(counter);
+//      char text[16];
+ char text[32] = "";
+      txt.toCharArray(text, 16);
+      radio.write(&text, sizeof(text));
+      Serial.println("Data Sent");
+ 
+  }
+}
+  
+  printTimer("RED", 0);
+    
+
+  radio.openReadingPipe(0, address_Mega2Uno);
+  radio.startListening();
+  if (radio.available()) {
+    while(radio.available()){
+      
+    overrideState = false;
+
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+    String txt1 = "COLOR: GREEN";
+    String txt2 = "COUNTER: " + String(timer);
+    timer = 10;
+
+    digitalWrite(greenLed, LOW);
+    digitalWrite(redLed, HIGH);
+    for(int x = timer; x >= 0; x = x - 1){
+      String txt2 = "COUNTER: " + String(10);
+      printTimer("GREEN", x);
+      delay(1000);
+    }
+    digitalWrite(greenLed, HIGH);
+    digitalWrite(redLed, LOW);
+
+      previousMillis = currentMillis;
+     overrideState = true;
+
+ radio.stopListening();
+     String txt = "COUNTER: " + String(counter);
+//      char text[16];
+      txt.toCharArray(text, 16);
+      radio.write(&text, sizeof(text));
+      Serial.println("Data Sent");
+    
+    
+
+    
+    }
+  
+
+  }
+
+  
+}
