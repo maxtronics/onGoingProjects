33a4a8dc8a201bde2a55264d13683b6ef7c8281f
maxtronics <50552423+maxtronics@users.noreply.github.com>
2019-06-23 12:49:09 +0800

    Add files via upload

---
+++ scan_mode/scan_mode.ino
@@ -0,0 +1,240 @@
+//build 2 modes
+//1. Scan / Enroll Mode
+//2. Tap Mode
+
+#include <SoftwareSerial.h>
+#include <FPM.h>
+
+SoftwareSerial fserial(A8, A10);
+
+FPM finger(&fserial);
+FPM_System_Params params;
+
+bool isDirectionPrinted = false;
+
+void setup() {
+  // put your setup code here, to run once:
+
+   Serial.begin(9600);
+
+    Serial.println("ENROLL test");
+    fserial.begin(57600);
+
+    if (finger.begin()) {
+        finger.readParams(&params);
+        Serial.println("Found fingerprint sensor!");
+        Serial.print("Capacity: "); Serial.println(params.capacity);
+        Serial.print("Packet length: "); Serial.println(FPM::packet_lengths[params.packet_len]);
+    } else {
+        Serial.println("Did not find fingerprint sensor :(");
+        while (1) yield();
+    }
+
+}
+
+uint16_t readnumber(void) {
+  uint16_t num = 0;
+  
+  while (num == 0) {
+    while (! Serial.available());
+    num = Serial.parseInt();
+  }
+  return num;
+}
+
+
+
+  
+int16_t enroll_finger(int16_t fid) {
+    int16_t p = -1;
+    Serial.println("Waiting for valid finger to enroll");
+    while (p != FPM_OK) {
+        p = finger.getImage();
+        switch (p) {
+            case FPM_OK:
+                Serial.println("Image taken");
+                break;
+            case FPM_NOFINGER:
+                Serial.println(".");
+                break;
+            case FPM_PACKETRECIEVEERR:
+                Serial.println("Communication error");
+                break;
+            case FPM_IMAGEFAIL:
+                Serial.println("Imaging error");
+                break;
+            case FPM_TIMEOUT:
+                Serial.println("Timeout!");
+                break;
+            case FPM_READ_ERROR:
+                Serial.println("Got wrong PID or length!");
+                break;
+            default:
+                Serial.println("Unknown error");
+                break;
+        }
+        yield();
+    }
+    // OK success!
+
+    p = finger.image2Tz(1);
+    switch (p) {
+        case FPM_OK:
+            Serial.println("Image converted");
+            break;
+        case FPM_IMAGEMESS:
+            Serial.println("Image too messy");
+            return p;
+        case FPM_PACKETRECIEVEERR:
+            Serial.println("Communication error");
+            return p;
+        case FPM_FEATUREFAIL:
+            Serial.println("Could not find fingerprint features");
+            return p;
+        case FPM_INVALIDIMAGE:
+            Serial.println("Could not find fingerprint features");
+            return p;
+        case FPM_TIMEOUT:
+            Serial.println("Timeout!");
+            return p;
+        case FPM_READ_ERROR:
+            Serial.println("Got wrong PID or length!");
+            return p;
+        default:
+            Serial.println("Unknown error");
+            return p;
+    }
+
+    Serial.println("Remove finger");
+    delay(2000);
+    p = 0;
+    while (p != FPM_NOFINGER) {
+        p = finger.getImage();
+        yield();
+    }
+
+    p = -1;
+    Serial.println("Place same finger again");
+    while (p != FPM_OK) {
+        p = finger.getImage();
+        switch (p) {
+            case FPM_OK:
+                Serial.println("Image taken");
+                break;
+            case FPM_NOFINGER:
+                Serial.print(".");
+                break;
+            case FPM_PACKETRECIEVEERR:
+                Serial.println("Communication error");
+                break;
+            case FPM_IMAGEFAIL:
+                Serial.println("Imaging error");
+                break;
+            case FPM_TIMEOUT:
+                Serial.println("Timeout!");
+                break;
+            case FPM_READ_ERROR:
+                Serial.println("Got wrong PID or length!");
+                break;
+            default:
+                Serial.println("Unknown error");
+                break;
+        }
+        yield();
+    }
+
+    // OK success!
+
+    p = finger.image2Tz(2);
+    switch (p) {
+        case FPM_OK:
+            Serial.println("Image converted");
+            break;
+        case FPM_IMAGEMESS:
+            Serial.println("Image too messy");
+            return p;
+        case FPM_PACKETRECIEVEERR:
+            Serial.println("Communication error");
+            return p;
+        case FPM_FEATUREFAIL:
+            Serial.println("Could not find fingerprint features");
+            return p;
+        case FPM_INVALIDIMAGE:
+            Serial.println("Could not find fingerprint features");
+            return p;
+        case FPM_TIMEOUT:
+            Serial.println("Timeout!");
+            return false;
+        case FPM_READ_ERROR:
+            Serial.println("Got wrong PID or length!");
+            return false;
+        default:
+            Serial.println("Unknown error");
+            return p;
+    }
+
+
+    // OK converted!
+    p = finger.createModel();
+    if (p == FPM_OK) {
+        Serial.println("Prints matched!");
+    } else if (p == FPM_PACKETRECIEVEERR) {
+        Serial.println("Communication error");
+        return p;
+    } else if (p == FPM_ENROLLMISMATCH) {
+        Serial.println("Fingerprints did not match");
+        return p;
+    } else if (p == FPM_TIMEOUT) {
+        Serial.println("Timeout!");
+        return p;
+    } else if (p == FPM_READ_ERROR) {
+        Serial.println("Got wrong PID or length!");
+        return p;
+    } else {
+        Serial.println("Unknown error");
+        return p;
+    }
+
+    Serial.print("ID "); Serial.println(fid);
+    p = finger.storeModel(fid);
+    if (p == FPM_OK) {
+        Serial.println("Stored!");
+        return 0;
+    } else if (p == FPM_PACKETRECIEVEERR) {
+        Serial.println("Communication error");
+        return p;
+    } else if (p == FPM_BADLOCATION) {
+        Serial.println("Could not store in that location");
+        return p;
+    } else if (p == FPM_FLASHERR) {
+        Serial.println("Error writing to flash");
+        return p;
+    } else if (p == FPM_TIMEOUT) {
+        Serial.println("Timeout!");
+        return p;
+    } else if (p == FPM_READ_ERROR) {
+        Serial.println("Got wrong PID or length!");
+        return p;
+    } else {
+        Serial.println("Unknown error");
+        return p;
+    }
+}
+
+void loop() {
+
+  if(!isDirectionPrinted){
+    Serial.println("Please enter ID number where you want to store Finger Print Data");
+    isDirectionPrinted = !isDirectionPrinted;
+  }
+    
+  if(Serial.available() > 0){
+    
+      int16_t fid = readnumber();
+
+      enroll_finger(fid);
+      isDirectionPrinted = !isDirectionPrinted;
+  }
+  }
+
+
